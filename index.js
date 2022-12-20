16cf7d5320a390607ce771ca21041f6957ce635a
zjuygp <zjuygp@gmail.com>
2022-12-20 19:34:55 +0800

    add config

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
+import "./lib/utils/config.js";
 import Koa from 'koa';
+import {log} from "./lib/utils/api_log.js";
 import setCommon from  "./lib/middleware/setCommon.js";
 import setRouter from "./lib/middleware/setRouter.js";
-import {log} from "./lib/utils/api_log.js";
-
-import dotenv from "dotenv";
-dotenv.config();
-let api_token = await import("./lib/utils/api_token.js");
-//要用上面的异步，在里面import config from "config";才不会报错
-//错误做法: import {verifyToken} from "./lib/utils/api_token.js"; 
+import bodyParser from "koa-bodyparser";
+import { verifyToken } from "./lib/utils/api_token.js";
 
 const app = new Koa();
 
 await setCommon(app);
-
 await setRouter(app, '/lib/router/client_no_login/');
 
 app.use(async (ctx, next) => {
-    if(api_token.verifyToken(ctx)){
+    if(verifyToken(ctx)){
         await next();
     }
 })
